8f5a7876ee9de3111b8364c9fb5ef53d8ee31e0e
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-06-04 12:30:25 +0530

    Update __main__.py

--- plugins/utils/imdb/__main__.py
+++ plugins/utils/imdb/__main__.py
@@ -45,7 +45,7 @@ async def _imdb(message: Message):
     try:
         movie_name = message.input_str
         await message.edit(f"__searching IMDB for__ : `{movie_name}`")
-        response = await _get(imdb.API_ONE_URL.format(theuserge=movie_name))
+        response = await _get(imdb.API_ONE_URL+movie_name)
         srch_results = json.loads(response.text)
         mov_imdb_id = srch_results.get("d")[1].get("id")
         image_link, description = await get_movie_description(
