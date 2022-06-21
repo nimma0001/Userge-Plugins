291b6d718e61e21271f21b53752ad545d1df571d
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-06-21 17:49:19 +0530

    Update __init__.py

--- plugins/utils/imdb2/__init__.py
+++ plugins/utils/imdb2/__init__.py
@@ -5,4 +5,4 @@
 API_ONE_URL = "http://betterimdbot.herokuapp.com/search.php?_={theuserge}"
 API_TWO_URL = "https://i-m-d-b.herokuapp.com/?tt={imdbttid}"
 API_THREE_URL = "https://justwatch.imdbot.workers.dev/?q={q}&L={L}"
-WATCH_COUNTRY = "en_IN
+WATCH_COUNTRY = "en_IN"
