29e2db2356b796787f38c204493ee3d8d482c8fe
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-06-21 18:05:20 +0530

    Update __main__.py

--- plugins/utils/imdb2/__main__.py
+++ plugins/utils/imdb2/__main__.py
@@ -45,7 +45,7 @@ async def _imdb(message: Message):
     try:
         movie_name = message.input_str
         await message.edit(f"__searching IMDB for__ : `{movie_name}`")
-        response = await _get(imdb.API_ONE_URL.format(theuserge=movie_name))
+        response = await _get("https://betterimdbot.herokuapp.com/search.php?_="+movie_name)
         srch_results = json.loads(response.text)
         mov_imdb_id = srch_results.get("d")[0].get("id")
         image_link, description = await get_movie_description(
@@ -54,7 +54,7 @@ async def _imdb(message: Message):
     except (IndexError, json.JSONDecodeError, AttributeError):
         movie_name = message.input_str
         await message.edit(f"__searching IMDB for__ : `{movie_name}`")
-        response = await _get(imdb.API_ONE_URL.format(theuserge=movie_name))
+        response = await _get(https://betterimdbot.herokuapp.com/search.php?_="+movie_name)
         srch_results = json.loads(response.text)
         mov_imdb_id = srch_results.get("d")[1].get("id")
         image_link, description = await get_movie_description(
