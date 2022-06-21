e25670587bc768315d2e2ccde379c6d0a25d3262
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-06-21 17:32:05 +0530

    Create __init__.py

---
+++ plugins/utils/imdb2/__init__.py
@@ -0,0 +1,8 @@
+# All rights reserved.
+
+""" search movies/tv series in imdb """
+
+API_ONE_URL = "http://betterimdbot.herokuapp.com/search.php?_={theuserge}"
+API_TWO_URL = "http://betterimdbot.herokuapp.com/?tt={imdbttid}"
+API_THREE_URL = "https://justwatch.imdbot.workers.dev/?q={q}&L={L}"
+WATCH_COUNTRY = "en_IN
