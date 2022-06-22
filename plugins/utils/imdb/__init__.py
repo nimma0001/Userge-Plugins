1e96383321794797c444d6a30e64a9cffc8f7320
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-06-22 17:41:20 +0530

    Update __init__.py

--- plugins/utils/imdb/__init__.py
+++ plugins/utils/imdb/__init__.py
@@ -1,14 +1,4 @@
-# Copyright (C) 2020-2022 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
-#
-# This file is part of < https://github.com/UsergeTeam/Userge > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/UsergeTeam/Userge/blob/master/LICENSE >
-#
-# All rights reserved.
-
-""" search movies/tv series in imdb """
-
-import os
+# import os
 
 API_ONE_URL = os.environ.get("IMDB_API_ONE_URL")
 API_TWO_URL = os.environ.get("IMDB_API_TWO_URL")
