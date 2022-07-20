5cdf7acf31bafe70a6e302f9259884ca7d887cc4
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-07-20 22:51:25 +0530

    Update __main__.py

--- plugins/utils/imdb/__main__.py
+++ plugins/utils/imdb/__main__.py
@@ -146,7 +146,7 @@ async def get_movie_description(imdb_id, max_length):
 <b>Story Line : </b><em>{story_line}</em>
 <b>Available On : 👇👇👇👇 </b>"""
 
-    povas = await search_jw()
+    povas = await search_jw(mov_name, imdb.WATCH_COUNTRY)
     if len(description + povas) > max_length:
         inc = max_length - len(description + povas)
         description = description[:inc - 3].strip() + "..."
@@ -300,9 +300,9 @@ if userge.has_bot:
         inline_query.stop_propagation()
 
 
-async def search_jw():
+async def search_jw(movie_name: str, locale: str):
     m_t_ = ""
-    response = await _get("https://justwatch.imdbot.workers.dev/?q={movie_name}&L={locale}")
+    response = await _get("https://justwatch.imdbot.workers.dev/?q="+movie_name+"&L="+locale)
     soup = json.loads(response.text)
     items = soup["items"]
     for item in items:
