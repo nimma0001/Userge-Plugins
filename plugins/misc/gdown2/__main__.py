1ef548a4af48494cb38a73868499a6abfe39419c
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-12-28 21:44:03 +0530

    Update __main__.py

--- plugins/misc/gdown2/__main__.py
+++ plugins/misc/gdown2/__main__.py
@@ -10,15 +10,16 @@ from datetime import date
 
 @userge.on_start
 async def _init() -> None:
-    gauth = GoogleAuth()
-    gauth.LoadCredentialsFile('/app/token.json')
-    drive = GoogleDrive(gauth)
+    pass
 
 @userge.on_cmd("gdown2", about="my first command")
 async def first_command(message: Message) -> None:
     """ this thing will be used as command doc string """
     message
     today = date.today().strftime("%d")
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile('/app/token.json')
+    drive = GoogleDrive(gauth)
     await message.edit(message.input_str.split('=')[1])
     file = drive.CreateFile({'id': message.input_str.split('=')[1]})
     name = file['title']
