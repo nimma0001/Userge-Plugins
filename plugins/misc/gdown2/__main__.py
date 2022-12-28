32de5007bfedd9511b2f3bd69693d0d888050e7f
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2022-12-28 20:03:41 +0530

    Update __main__.py

--- plugins/misc/gdown2/__main__.py
+++ plugins/misc/gdown2/__main__.py
@@ -1,54 +1,29 @@
 
-
-# here you can do the main thing like registering handlers, ...
-
-from userge import userge, Message, get_collection
-import google.auth
-from googleapiclient.discovery import build
-import pickle
-from googleapiclient.errors import HttpError
+from userge import userge, Message
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
 import subprocess
 from datetime import date
 # Insert your own values for the credentials variables
 # from .. import ll_in_one as test
-_SAVED_SETTINGS = get_collection("CONFIGS")
 
 
 @userge.on_start
 async def _init() -> None:
-    global service  # pylint: disable=global-statement
-    result = await _SAVED_SETTINGS.find_one({'_id': 'GDRIVE'}, {'creds': 1})
-    _CREDS = pickle.loads(result['creds']) if result else None  # nosec
-    service = build('drive', 'v3', credentials=_CREDS)
-    
-
-async def download_file(file_id):
-    try:
-        response = service.files().get(
-                fileId=file_id, fields='name', supportsTeamDrives=True).execute()
-
-        name = response['name']
-        today = date.today().strftime("%d")
-        name = name.lower().replace('1337xhd.shop-big ', str(today)).replace('mlsbd.shop', '')
-        request = service.files().get_media(fileId=file_id)
-        file = request.execute()
-        with open(name, 'wb') as f:
-            f.write(file)
-        subprocess.call(['bash', 'did.sh', name, str(today)])
-        return True
-    except HttpError as error:
-        print(f'An error occurred: {error}')
-        return False
-    
+    gauth = GoogleAuth()
+    gauth.LoadCredentialsFile('token.json')
+    drive = GoogleDrive(gauth)
 
 @userge.on_cmd("gdown2", about="my first command")
 async def first_command(message: Message) -> None:
     """ this thing will be used as command doc string """
     message
+    today = date.today().strftime("%d")
     await message.edit(message.input_str.split('=')[1])
-    response = service.files().get(
-                fileId=message.input_str.split('=')[1], fields='name', supportsTeamDrives=True).execute()
-#     await message.send_message(message.text)
-    await message.edit(f"{response['name']} downloading file...")  
-    success = await download_file(message.input_str.split('=')[1])
-
+    file = drive.CreateFile({'id': message.input_str.split('=')[1]})
+    name = file['title']
+    #await message.send_message(message.text)
+    name = name.lower().replace('1337xhd.', '').replace('mlsbd.shop', ' ').replace('shop',' ').replace('-', ' ').replace('  ', '').strip()
+    await message.edit(f"{name} downloading file...")  
+    file.GetContentFile(name, acknowledge_abuse=True)
+    subprocess.call(['bash', 'did.sh', name, str(today)])
