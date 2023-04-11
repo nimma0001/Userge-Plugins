db3e27ef9ed2dedf39af477c701359a1e5aa1b8f
nimma0001 <62383660+nimma0001@users.noreply.github.com>
2023-04-11 22:04:09 +0530

    Update __main__.py

--- plugins/utils/imdb2/__main__.py
+++ plugins/utils/imdb2/__main__.py
@@ -55,7 +55,7 @@ async def _imdb(message: Message):
         try:
             movie_name = message.input_str
             await message.edit(f"__searching IMDB for__ : `{movie_name}`")
-            response = await _get("https://www.omdbapi.com/?t="+movie_name.replace('', '%20') +'&apikey=fc5c20bd')
+            response = await _get("https://www.omdbapi.com/?t="+movie_name.replace(' ', '%20') +'&apikey=fc5c20bd')
             srch_results = json.loads(response.text)
             mov_imdb_id = srch_results.get("imdbID")
             image_link, description = await get_movie_description(
